50d62bbdc480f46a5ba01866f74c79b61b39b250
Kees Postma <kk.postma@gmail.com>
2017-07-19 09:39:45 +0100

    Changes active menu item based on scroll and improves responsiveness

--- animation.js
+++ animation.js
@@ -1,32 +1,37 @@
-$(document).ready(function(){
-  // $(document).on("scroll", onScroll);
+$(document).ready(function () {
+		$(document).on("scroll", onScroll);
 
-  // Works the menu tabs on the left of the page
-  $('.nav-tab').on('click', function(){
-    $('.nav-tab').removeClass('active');
-    $(this).addClass('active');
-  });
+		$('a[href^="#"]').on('click', function (e) {
+			e.preventDefault();
+			$(document).off("scroll");
 
-  // Smooth scrol
-  $('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-    }, 300);
-    return false;
-  });
-})
+			$('a').each(function () {
+				$(this).removeClass('active');
+			})
+			$(this).addClass('active');
 
-// function onScroll(){
-//   var scrollPos = $(document).scrollTop();
-//   $('.nav-tabs a').each(function () {
-//       var currLink = $(this);
-//       var refElement = $(currLink.attr("href"));
-//       if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-//           $('.nav-tab').removeClass("active");
-//           currLink.children('div').addClass("active");
-//       }
-//       else{
-//           currLink.children('div').removeClass("active");
-//       }
-//   });
-// };
+			var target = this.hash;
+			$target = $(target);
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top-30
+			}, 500, 'swing', function () {
+				window.location.hash = target;
+				$(document).on("scroll", onScroll);
+			});
+		});
+	});
+
+	function onScroll(event){
+		var scrollPosition = $(document).scrollTop();
+		$('nav a').each(function () {
+			var currentLink = $(this);
+			var refElement = $(currentLink.attr("href"));
+			if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+				$('nav ul li a').removeClass("active");
+				currentLink.addClass("active");
+			}
+			else{
+				currentLink.removeClass("active");
+			}
+		});
+	}
